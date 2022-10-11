ed7175c9d360c21aa0bc78482b9ee887662316d7
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-11 14:30:12 +0900

    setting

---
+++ src/main/java/com/ll/exam/melon_exam/MelonExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.melon_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelonExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelonExamApplication.class, args);
+    }
+
+}
