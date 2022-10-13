fc50fdfc113c7c92ac54e486f56a88590f262133
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-13 13:04:54 +0900

    Feat : Pay Form

--- src/main/java/com/ll/exam/melon_exam/app/order/controller/OrderController.java
+++ src/main/java/com/ll/exam/melon_exam/app/order/controller/OrderController.java
@@ -3,11 +3,13 @@ package com.ll.exam.melon_exam.app.order.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.exam.melon_exam.app.member.entity.Member;
+import com.ll.exam.melon_exam.app.member.service.MemberService;
 import com.ll.exam.melon_exam.app.order.entity.Order;
 import com.ll.exam.melon_exam.app.order.exception.OrderIdNotMatchedException;
 import com.ll.exam.melon_exam.app.order.service.OrderService;
 import com.ll.exam.melon_exam.app.security.dto.MemberContext;
 import com.ll.exam.melon_exam.app.order.exception.ActorCanNotSeeOrderException;
+import com.ll.exam.melon_exam.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.http.client.ClientHttpResponse;
@@ -34,6 +36,7 @@ public class OrderController {
     private final OrderService orderService;
     private final RestTemplate restTemplate = new RestTemplate();
     private final ObjectMapper objectMapper;
+    private final MemberService memberService;
 
     @GetMapping("/{id}")
     @PreAuthorize("isAuthenticated()")
@@ -42,11 +45,14 @@ public class OrderController {
 
         Member actor = memberContext.getMember();
 
+        long restCash = memberService.getRestCash(actor);
+
         if (orderService.actorCanSee(actor, order) == false) {
             throw new ActorCanNotSeeOrderException();
         }
 
         model.addAttribute("order", order);
+        model.addAttribute("actorRestCash", restCash);
 
         return "order/detail";
     }
@@ -99,10 +105,9 @@ public class OrderController {
                 "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
 
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            JsonNode successNode = responseEntity.getBody();
-            model.addAttribute("orderId", successNode.get("orderId").asText());
-            String secret = successNode.get("secret").asText(); // 가상계좌의 경우 입금 callback 검증을 위해서 secret을 저장하기를 권장함
-            return "order/success";
+            orderService.payByTossPayments(order);
+
+            return "redirect:/order/%d?msg=%s".formatted(order.getId(), Ut.url.encode("결제가 완료되었습니다."));
         } else {
             JsonNode failNode = responseEntity.getBody();
             model.addAttribute("message", failNode.get("message").asText());
